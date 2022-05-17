5939b0a0b1dcb062e76907ce971be72e30184164
GAYA <Gayay@academy-skills.com>
2022-05-17 12:04:39 +0300

    added description to enum variables

--- src/character_vars/Type.java
+++ src/character_vars/Type.java
@@ -3,8 +3,10 @@ package character_vars;
 public enum Type {
     BLAST(1,2), COMBAT(2,3), SPEED(3,1), UNIVERSAL(0,null);
 
-    private Integer id;
-    private Integer against;
+    //my id
+    private final Integer id;
+    //the id of the type I have advantage against
+    private final Integer against;
 
     Type(Integer id, Integer against) {
         this.id = id;
@@ -14,7 +16,13 @@ public enum Type {
     public Integer getId() {
         return id;
     }
+
+    /**
+     *
+     * @param otherType - a Type instance to check
+     * @return if I have advantage against the given type ("otherType")
+     */
     public boolean isAgainst(Type otherType) {
-        return (against==null)? false: (against==otherType.getId());
+        return (against!=null)&&(against.equals(otherType.getId()));
     }
 }
